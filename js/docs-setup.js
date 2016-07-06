0d280ca36acf83e0653a091acad397e50a4f67e6
Henry Miskin <henry.miskin@talktalk.net>
2016-07-06 17:18:44 +0100

    Added documentation

---
+++ js/docs-setup.js
@@ -0,0 +1,153 @@
+NG_DOCS={
+  "sections": {
+    "client": "Client Documentation",
+    "server": "Server Documentation"
+  },
+  "pages": [
+    {
+      "section": "client",
+      "id": "turingLearningApp",
+      "shortName": "turingLearningApp",
+      "type": "overview",
+      "moduleName": "turingLearningApp",
+      "shortDescription": "TuringLab learning application",
+      "keywords": "angular-sortable-view angularjs angularscratchblocks anguvideo application btford client codemirror github http https io learning markdown nganimate ngcookies ngfileupload ngmaterial ngresource ngsanitize ngstorage ngtable ngtagsinput org overview router turinglab turinglearningapp ui utilities"
+    },
+    {
+      "section": "client",
+      "id": "turingLearningApp.directive:brickCard",
+      "shortName": "brickCard",
+      "type": "directive",
+      "moduleName": "turingLearningApp",
+      "shortDescription": "Directive used to display a brick. The brick will be displayed with a header",
+      "keywords": "$scope action attached brick called clicked client directive display displayed footer function header interactions number object openbrick purpose service signal turinglearningapp"
+    },
+    {
+      "section": "client",
+      "id": "turingLearningApp.directive:developmentEnvironment",
+      "shortName": "developmentEnvironment",
+      "type": "directive",
+      "moduleName": "turingLearningApp",
+      "shortDescription": "Displays the embedded development environment or editor and updated the",
+      "keywords": "$timeout angularjs client development directive display displays editor embedded environment https org service turinglearningapp updated updates"
+    },
+    {
+      "section": "client",
+      "id": "turingLearningApp.directive:dropdownInput",
+      "shortName": "dropdownInput",
+      "type": "directive",
+      "moduleName": "turingLearningApp",
+      "shortDescription": "Dropdown input to dispaly a list of options which can be selected. When a",
+      "keywords": "callback called chanaged changed client currently directive dispaly dropdown function input label left list options parameter selected turinglearningapp"
+    },
+    {
+      "section": "client",
+      "id": "turingLearningApp.directive:roundButton",
+      "shortName": "roundButton",
+      "type": "directive",
+      "moduleName": "turingLearningApp",
+      "shortDescription": "Simple round button with given icon and text which will call click",
+      "keywords": "button call called class click clicked client directive dispaly function icon label round simple text turinglearningapp"
+    },
+    {
+      "section": "client",
+      "id": "turingLearningApp.directive:textInput",
+      "shortName": "textInput",
+      "type": "directive",
+      "moduleName": "turingLearningApp",
+      "shortDescription": "Dropdown input to dispaly a list of options which can be selected. When a",
+      "keywords": "callback called chanaged client directive dispaly dropdown edited format highlighting input list options parameter plain selected syntax text turinglearningapp"
+    },
+    {
+      "section": "client",
+      "id": "turingLearningApp.directive:toggleInput",
+      "shortName": "toggleInput",
+      "type": "directive",
+      "moduleName": "turingLearningApp",
+      "shortDescription": "Toggle input which which clicked chanages the value of ngModel",
+      "keywords": "boolean chanages clicked client directive input ngmodel toggle turinglearningapp"
+    },
+    {
+      "section": "client",
+      "id": "turingLearningApp.service:$Resource",
+      "shortName": "$Resource",
+      "type": "service",
+      "moduleName": "turingLearningApp",
+      "shortDescription": "The $Resource service returns an extension of the $resource service which is used to communicate with a RESTful endpoint",
+      "keywords": "$resource access add angularjs apply associate attributes callback called circular client communicate convert copy creating data define delete designed endpoint error existing extension filter https inherit json list method object objects org overridden parameters params persisting prepare promises prototype query recursive references remote resource resources restful returned returning returns save saving service single success sued transform turinglearningapp updating"
+    },
+    {
+      "section": "client",
+      "id": "turingLearningApp.service:Authorization",
+      "shortName": "Authorization",
+      "type": "service",
+      "moduleName": "turingLearningApp",
+      "shortDescription": "The Authorization service can be used to authorise a state change. It is assumes that user roles are ordered and that higher users can access routes for users with lower privileges.",
+      "keywords": "$rootscope $state access angularjs assumes authorise authorization authorize change check client comparing currently github higher http https io logged lower method ordered org privileges promise role roles route router routes service turinglearningapp user users"
+    },
+    {
+      "section": "client",
+      "id": "turingLearningApp.service:Environment",
+      "shortName": "Environment",
+      "type": "service",
+      "moduleName": "turingLearningApp",
+      "shortDescription": "Service used to control the development environment which is also known as the editor. This service is used to receive changes in size experienced by the developmentPlaceholder and communicate the required size to the  developmentEnvironment",
+      "keywords": "_listener callback called calls change changed changes client communicate control defines development developmentenvironment developmentplaceholder directive editor environment experienced function height listener method notify receive required service set_environment_listener size source turinglearningapp update_size update_source updates url width"
+    },
+    {
+      "section": "client",
+      "id": "turingLearningApp.service:History",
+      "shortName": "History",
+      "type": "service",
+      "moduleName": "turingLearningApp",
+      "shortDescription": "Service used to record the states which have been visited using the learning system to allow users to navigate back up the tree and know the parent of a given brick",
+      "keywords": "$localstorage $state active add allow brick change changes client current github history http https io learning method navigate notify parameters params parent phantom previous provided record reload replaces reset return router service set system tree turinglearningapp users visited"
+    },
+    {
+      "section": "client",
+      "id": "turingLearningApp.service:Principal",
+      "shortName": "Principal",
+      "type": "service",
+      "moduleName": "turingLearningApp",
+      "shortDescription": "The Principal service can be used to authorise a state change. It is assumes that user roles are ordered and that higher users can access routes for users with lower privileges.",
+      "keywords": "$cookies $http $q access admin angularjs assumes authenticate authenticated authorise callback called change check checking client create created createuser currentuser delete getcurrentuser gettoken higher https info instructor isadmin isinstructor isloggedin isloggedinasync logged login logout lower method ordered org password principal privileges promise resolve return roles routes save service token true turinglearningapp user username users waits"
+    },
+    {
+      "section": "client",
+      "id": "turingLearningApp.service:Sidebar",
+      "shortName": "Sidebar",
+      "type": "service",
+      "moduleName": "turingLearningApp",
+      "shortDescription": "The Sidebar service can be used to control the sidebar view",
+      "keywords": "client control display method service sidebar toggle turinglearningapp view"
+    },
+    {
+      "section": "client",
+      "id": "turingLearningApp.service:Uploader",
+      "shortName": "Uploader",
+      "type": "service",
+      "moduleName": "turingLearningApp",
+      "shortDescription": "Service to upload images to amazon web services.",
+      "keywords": "$http $q address amazon angularjs client create file firstly https images method org posts promise request returned service services signed turinglearningapp upload url web"
+    },
+    {
+      "section": "client",
+      "id": "utilities",
+      "shortName": "utilities",
+      "type": "overview",
+      "moduleName": "utilities",
+      "shortDescription": "TuringLab utilities",
+      "keywords": "ang-drag-drop client github http io overview turinglab utilities"
+    }
+  ],
+  "apis": {
+    "client": false,
+    "server": false
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.js"
+  ]
+};
